5fb4b73d86d290cb935d6086f6ef7219539fad43
jb-delafosse <jean-baptiste@lumapps.com>
2021-02-18 21:50:42 +0100

    feat(cli): add output-dir option in CLI

--- marko_backlinks/usecases/write.py
+++ marko_backlinks/usecases/write.py
@@ -9,8 +9,9 @@ from marko_backlinks.interfaces import renderer
 
 
 def write(files: Dict[Note, Document], directory: Path) -> None:
+    directory.mkdir(parents=True, exist_ok=True)
     for note, ast in files.items():
-        with open(os.path.join(directory, note.note_path), "w") as file:
+        with open(os.path.join(directory, note.note_path), "w+") as file:
             with renderer.RENDERER as _renderer:
                 text = _renderer.render(ast)
                 file.write(text)
